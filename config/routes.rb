0226d24fd674d047f88e5d57f104b86941f8ccda
lukikow <lukikow@gmail.com>
2017-05-05 20:59:27 +0200

    create categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#index'
 
-  get '/about', to: 'homepage#about'
+  resources :categories
 
+  get '/about', to: 'homepage#about'
   get '/cookies', to: 'homepage#cookies'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
