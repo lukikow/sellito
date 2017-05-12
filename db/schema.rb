228f76f205d431fdf12145250d54e04f1daff410
lukikow <lukikow@gmail.com>
2017-05-12 19:09:42 +0200

    added new table Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506160632) do
+# Jesli zapomni sie o polu to mozna rake db:rollback albo
+# zrobic nowa migracje: rails g migration NazwaMigracji, dopisac w niej dla jakiej klasy co ma zrobic
+# np:
+#class AddIsAdminToUsers < ActiveRecord::Migration[5.0]
+#  def change
+#    add_column :users, :is_admin?, :boolean, default: false
+#  end
+#end
+
+ActiveRecord::Schema.define(version: 20170512165101) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "expiration_date"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
